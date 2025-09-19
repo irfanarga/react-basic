d33bb425534d6058c5bb03cb91cf06dde2f123ea
irfanarga <irfanargaprasetya81@gmail.com>
2025-09-19 09:37:00 +0700

    pure component

--- src/hello-world/main.jsx
+++ src/hello-world/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from "react-dom/client";
 import HelloWorld from "./HelloWorld.jsx";
 import Container from "./Container.jsx";
 import TodoList from "../todolist/TodoList.jsx";
+import Table from "../table/Table.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Container>
       <HelloWorld />
       <TodoList />
+      <Table />
     </Container>
   </StrictMode>
 );
--- src/todolist/TodoList.jsx
+++ src/todolist/TodoList.jsx
@@ -1,12 +1,40 @@
 import Todo from "./Todo.jsx";
 
 export default function TodoList() {
+  const data = [
+    {
+      id: 0,
+      text: "Learn HTML",
+      isCompleted: true,
+      isDeleted: true,
+    },
+    {
+      id: 1,
+      text: "Learn CSS",
+      isCompleted: true,
+    },
+    {
+      id: 2,
+      text: "Learn JavaScript",
+      isCompleted: true,
+    },
+    {
+      id: 3,
+      text: "Learn React",
+      isCompleted: false,
+    },
+    {
+      id: 4,
+      text: "Learn ReactJS Router",
+      isCompleted: false,
+    },
+  ];
+
   return (
     <ul>
-      <Todo isCompleted={true} text="Learn HTML" isDeleted={true} />
-      <Todo isCompleted={true} text="Learn CSS" />
-      <Todo isCompleted={true} text="Learn JavaScript" />
-      <Todo isCompleted={false} text="Learn React" />
+      {data.map((todo) => (
+        <Todo key={todo.id} {...todo} />
+      ))}
     </ul>
   );
 }
