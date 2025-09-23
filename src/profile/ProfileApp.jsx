30aeba0e6ffe218ca0b0179e4ec77887ca2b93f4
irfanarga <irfanargaprasetya81@gmail.com>
2025-09-23 11:41:59 +0700

    context integrate with state

--- src/profile/ProfileApp.jsx
+++ src/profile/ProfileApp.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import Profile from "./Profile.jsx";
 import ProfileAddress from "./ProfileAddress.jsx";
 import { ProfileContext } from "./ProfileContext.jsx";
+import ProfileForm from "./ProfileForm.jsx";
 
 export default function ProfileApp() {
+  const [name, setName] = useState("Irfan");
+
   return (
     <div>
-      <ProfileContext.Provider value="Irfan">
+      <ProfileContext.Provider value={name}>
         <h1>Profile App</h1>
+        <ProfileForm name={name} setName={setName} />
         <Profile />
         <ProfileAddress />
       </ProfileContext.Provider>
